c5c3157bf7d380687ef6a3b96f950410fd46eb8e
David Havel <davidek.havel@gmail.com>
2024-02-28 18:50:12 +0100

    it counts by 2!!

--- src/main/java/com/davidhavel/casovac9000/HelloController.java
+++ src/main/java/com/davidhavel/casovac9000/HelloController.java
@@ -1,6 +1,7 @@
 package com.davidhavel.casovac9000;
 
 import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.transform.Translate;
 import javafx.util.Duration;
@@ -15,7 +17,7 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class HelloController implements Initializable, Runnable {
 
     @FXML
     private ComboBox<Integer> comboHour;
@@ -30,6 +32,20 @@ public class HelloController implements Initializable {
 
     @FXML
     private AnchorPane settingsPage;
+
+    @FXML
+    private Label hourLabel;
+
+    @FXML
+    private Label minutesLabel;
+
+    @FXML
+    private Label secondsLabel;
+
+    private Thread t;
+
+    private boolean running = false;
+    private int seconds;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -49,6 +65,20 @@ public class HelloController implements Initializable {
 
     public void startCounter(ActionEvent event) {
         slideUp();
+        t = new Thread(this);
+        t.setDaemon(true);
+        running = true;
+
+        loadTime();
+        t.start();
+    }
+
+    private void loadTime() {
+     seconds = comboHour.getValue() * 3600 + comboMin.getValue() * 60 + comboSec.getValue();
+    }
+    public void stopCounter(ActionEvent event) {
+        slideDown();
+        running = false;
     }
 
     public void slideUp() {
@@ -61,11 +91,49 @@ public class HelloController implements Initializable {
     }
 
     public void slideDown() {
-TranslateTransition transition1 = new TranslateTransition();
+        TranslateTransition transition1 = new TranslateTransition();
         transition1.setDuration(Duration.millis(800));
         transition1.setToX(0);
-        transition1.setToY(380);
+        transition1.setToY(0);
         transition1.setNode(settingsPage);
         transition1.play();
+    }
+
+    @Override
+    public void run() {
+        long time = System.currentTimeMillis() + 1000;
+        while (running) {
+            if (System.currentTimeMillis() > time) {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateTime();
+                    }
+                });
+                if (seconds == 0) running = false;
+                else seconds--;
+                time = System.currentTimeMillis() + 1000;
+
+            }
+        }
+    }
+
+    private void updateTime() {
+        System.out.println(seconds--);
+        short hours = (short) (seconds / 3600);
+        hourLabel.setText((hours < 10) ? "0" + hours: "" + hours);
+        short minutes = (short) ((seconds % 3600) / 60);
+        minutesLabel.setText((minutes < 10) ? "0" + minutes: "" + minutes);
+        short sec = (short) (seconds % 60);
+        secondsLabel.setText((sec < 10) ? "0" + sec: "" + sec);
+    }
+
+    public void pauseCounter(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void resetCounter(ActionEvent event) {
+    loadTime();
     }
 }
